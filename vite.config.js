20a9dcc392572a0fa7ee2acae31f7453f4023ec4
Hdd5ps <hdd5ps@virginia.edu>
2025-01-07 17:11:49 +0000

    Set up GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   assetsInclude: ['**/*.glb']
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: '/index.html'
-    }
-  }
-});
+  assetsInclude: ['**/*.glb']
+})
