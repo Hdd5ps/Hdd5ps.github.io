902ccac5977f360a0353fa97776fcd19b1620ac4
Hdd5ps <hdd5ps@virginia.edu>
2025-01-07 19:06:56 +0000

    build and deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   assetsInclude: ['**/*.glb']
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
-})
+  assetsInclude: ['**/*.glb'],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/index.html'
+    }
+  }
+});
