74ecf2835881946ef05ea7766eb4341ebf0bebd8
Hdd5ps <hdd5ps@virginia.edu>
2025-01-08 18:47:18 +0000

    main.jsx to main.js

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -6,10 +6,10 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
         Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Emre</span>
+        <span className='font-semibold mx-2 text-white'>Kamal</span>
         👋
         <br />
-        A Software Engineer from Türkiye 
+        A Tech Enthusiast from Nepal 
       </h1>
     );
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,14 +15,14 @@ const About = () => {
         Hello, I'm{" "}
         <span className="blue-gradient_text font-semibold drop-shadow">
           {" "}
-          Emre
+          Kamal
         </span>{" "}
         👋
       </h1>
 
       <div className="mt-5 flex flex-col gap-3 text-slate-500">
         <p>
-          Software Engineer based in Türkiye, specializing in technical
+          Computer Science Student based in USA, specializing in technical
           education through hands-on learning and building applications.
         </p>
       </div>
