b9f7bdc055e14bcc3c70a0a1c8c34afcf794ff87
Hdd5ps <hdd5ps@virginia.edu>
2025-01-12 21:25:15 +0000

    new

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  assetsInclude: ['**/*.glb'],
-});
+})
